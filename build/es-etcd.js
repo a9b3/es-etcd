44a8516992c24fa0d46bcb917db876cb68ff5897
Sam Lau <esayemm@gmail.com>
2016-10-29 17:38:21 -0700

    1.1.3

--- build/es-etcd.js
+++ build/es-etcd.js
@@ -304,23 +304,34 @@ var EsEtcd = function () {
     }()
   }, {
     key: 'watch',
+    value: function watch(key, cb) {
+      var cancel = function cancel() {};
+      this._watch(key, cb, function (c) {
+        return cancel = c;
+      });
+      return function () {
+        return cancel();
+      };
+    }
+  }, {
+    key: 'mkdir',
     value: function () {
-      var _ref11 = _asyncToGenerator(regeneratorRuntime.mark(function _callee5(key, cb) {
-        var cancel;
+      var _ref11 = _asyncToGenerator(regeneratorRuntime.mark(function _callee5(key) {
+        var _ref12, data;
+
         return regeneratorRuntime.wrap(function _callee5$(_context5) {
           while (1) {
             switch (_context5.prev = _context5.next) {
               case 0:
-                cancel = function cancel() {};
+                _context5.next = 2;
+                return this._axiosInstance.put(this._getKeyUrl() + '/' + key, _querystring2.default.stringify({ dir: true }));
 
-                this._watch(key, cb, function (c) {
-                  return cancel = c;
-                });
-                return _context5.abrupt('return', function () {
-                  return cancel();
-                });
+              case 2:
+                _ref12 = _context5.sent;
+                data = _ref12.data;
+                return _context5.abrupt('return', data);
 
-              case 3:
+              case 5:
               case 'end':
                 return _context5.stop();
             }
@@ -328,40 +339,8 @@ var EsEtcd = function () {
         }, _callee5, this);
       }));
 
-      function watch(_x10, _x11) {
+      function mkdir(_x10) {
         return _ref11.apply(this, arguments);
-      }
-
-      return watch;
-    }()
-  }, {
-    key: 'mkdir',
-    value: function () {
-      var _ref12 = _asyncToGenerator(regeneratorRuntime.mark(function _callee6(key) {
-        var _ref13, data;
-
-        return regeneratorRuntime.wrap(function _callee6$(_context6) {
-          while (1) {
-            switch (_context6.prev = _context6.next) {
-              case 0:
-                _context6.next = 2;
-                return this._axiosInstance.put(this._getKeyUrl() + '/' + key, _querystring2.default.stringify({ dir: true }));
-
-              case 2:
-                _ref13 = _context6.sent;
-                data = _ref13.data;
-                return _context6.abrupt('return', data);
-
-              case 5:
-              case 'end':
-                return _context6.stop();
-            }
-          }
-        }, _callee6, this);
-      }));
-
-      function mkdir(_x12) {
-        return _ref12.apply(this, arguments);
       }
 
       return mkdir;
@@ -378,19 +357,51 @@ var EsEtcd = function () {
   }, {
     key: 'version',
     value: function () {
-      var _ref14 = _asyncToGenerator(regeneratorRuntime.mark(function _callee7() {
-        var _ref15, data;
+      var _ref13 = _asyncToGenerator(regeneratorRuntime.mark(function _callee6() {
+        var _ref14, data;
+
+        return regeneratorRuntime.wrap(function _callee6$(_context6) {
+          while (1) {
+            switch (_context6.prev = _context6.next) {
+              case 0:
+                _context6.next = 2;
+                return this._axiosInstance.get(this._getBaseUrl() + '/version');
+
+              case 2:
+                _ref14 = _context6.sent;
+                data = _ref14.data;
+                return _context6.abrupt('return', data);
+
+              case 5:
+              case 'end':
+                return _context6.stop();
+            }
+          }
+        }, _callee6, this);
+      }));
+
+      function version() {
+        return _ref13.apply(this, arguments);
+      }
+
+      return version;
+    }()
+  }, {
+    key: 'statsLeader',
+    value: function () {
+      var _ref15 = _asyncToGenerator(regeneratorRuntime.mark(function _callee7() {
+        var _ref16, data;
 
         return regeneratorRuntime.wrap(function _callee7$(_context7) {
           while (1) {
             switch (_context7.prev = _context7.next) {
               case 0:
                 _context7.next = 2;
-                return this._axiosInstance.get(this._getBaseUrl() + '/version');
+                return this._axiosInstance.get(this._getBaseUrl() + '/v' + this._version + '/stats/leader');
 
               case 2:
-                _ref15 = _context7.sent;
-                data = _ref15.data;
+                _ref16 = _context7.sent;
+                data = _ref16.data;
                 return _context7.abrupt('return', data);
 
               case 5:
@@ -401,28 +412,28 @@ var EsEtcd = function () {
         }, _callee7, this);
       }));
 
-      function version() {
-        return _ref14.apply(this, arguments);
+      function statsLeader() {
+        return _ref15.apply(this, arguments);
       }
 
-      return version;
+      return statsLeader;
     }()
   }, {
-    key: 'statsLeader',
+    key: 'statsSelf',
     value: function () {
-      var _ref16 = _asyncToGenerator(regeneratorRuntime.mark(function _callee8() {
-        var _ref17, data;
+      var _ref17 = _asyncToGenerator(regeneratorRuntime.mark(function _callee8() {
+        var _ref18, data;
 
         return regeneratorRuntime.wrap(function _callee8$(_context8) {
           while (1) {
             switch (_context8.prev = _context8.next) {
               case 0:
                 _context8.next = 2;
-                return this._axiosInstance.get(this._getBaseUrl() + '/v' + this._version + '/stats/leader');
+                return this._axiosInstance.get(this._getBaseUrl() + '/v' + this._version + '/stats/self');
 
               case 2:
-                _ref17 = _context8.sent;
-                data = _ref17.data;
+                _ref18 = _context8.sent;
+                data = _ref18.data;
                 return _context8.abrupt('return', data);
 
               case 5:
@@ -433,40 +444,8 @@ var EsEtcd = function () {
         }, _callee8, this);
       }));
 
-      function statsLeader() {
-        return _ref16.apply(this, arguments);
-      }
-
-      return statsLeader;
-    }()
-  }, {
-    key: 'statsSelf',
-    value: function () {
-      var _ref18 = _asyncToGenerator(regeneratorRuntime.mark(function _callee9() {
-        var _ref19, data;
-
-        return regeneratorRuntime.wrap(function _callee9$(_context9) {
-          while (1) {
-            switch (_context9.prev = _context9.next) {
-              case 0:
-                _context9.next = 2;
-                return this._axiosInstance.get(this._getBaseUrl() + '/v' + this._version + '/stats/self');
-
-              case 2:
-                _ref19 = _context9.sent;
-                data = _ref19.data;
-                return _context9.abrupt('return', data);
-
-              case 5:
-              case 'end':
-                return _context9.stop();
-            }
-          }
-        }, _callee9, this);
-      }));
-
       function statsSelf() {
-        return _ref18.apply(this, arguments);
+        return _ref17.apply(this, arguments);
       }
 
       return statsSelf;
